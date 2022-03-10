15a9bf009cfa294d4a982aac958f6e1885667e4b
Jonathan Gilroy-Powell <jonathan.gilroy-powell@and.digital>
2022-03-10 11:31:56 +0000

    removed demo test code

--- sessions/05_unhappy_path/01_error_handling/userRepository.test.ts
+++ sessions/05_unhappy_path/01_error_handling/userRepository.test.ts
@@ -9,19 +9,13 @@ brokenDatabase.mockImplementation(() => { throw new Error("broken database") });
 
 describe('error handling', () => {
   it('is a bad example of error handling', () => {
-    try {
-      getUser('foo');
-    }
-    catch (err) {
-      expect(err).toStrictEqual(new Error("Failed to access user foo"));
-    }
+
   });
 
   it('is a good example of error handling', () => {
-    expect(() => getUser('foo')).toThrow(new Error("Failed to access user foo"));
   });
 
   it('is another good example of error handling', () => {
-    expect(() => getUser('foo')).toThrow(/Failed to access user foo/);
+
   });
 });
